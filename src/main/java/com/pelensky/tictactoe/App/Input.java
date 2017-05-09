c307b3b1fb4b5973d30769a6f8c2f3f64e263419
Dan Pelensky <dan@pelensky.com>
2017-05-09 10:27:42 +0100

    Changed error handling for input validation

--- src/main/java/com/pelensky/tictactoe/App/Input.java
+++ src/main/java/com/pelensky/tictactoe/App/Input.java
@@ -4,37 +4,34 @@ import java.util.Scanner;
 
 public class Input {
 
-  private final Scanner input;
-  private final Print print;
+    private final Scanner input;
+    private final Print print;
 
-  public Input(Scanner input, Print print) {
-    this.input = input;
-    this.print = print;
-  }
-
-  public int getInteger() {
-    int integer = -1;
-    while (integer < 0) {
-      String selection = input.nextLine().trim();
-      if (validateInput(selection)) {
-        integer = Integer.parseInt(selection);
-      } else {
-        print.invalidSelection();
-      }
-    }
-    return integer;
-  }
-
-
-
-  private boolean validateInput(String text) {
-    try {
-      Integer.parseInt(text);
-      return true;
-    } catch (NumberFormatException e) {
-      return false;
+    public Input(Scanner input, Print print) {
+        this.input = input;
+        this.print = print;
     }
 
-  }
+    public int getInteger() {
+        int integer = -1;
+        while (integer < 0) {
+            String selection = input.nextLine().trim();
+            integer = validateInput(selection);
+            if (integer != -1) {
+                return integer;
+            } else {
+                print.invalidSelection();
+            }
+        }
+        return integer;
+    }
+
+    private int validateInput(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
 
 }
