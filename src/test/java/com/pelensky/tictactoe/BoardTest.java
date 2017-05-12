60bb3bb6324b22f3dc9d19966d213ef0ebb8741b
Dan Pelensky <dan@pelensky.com>
2017-05-12 12:04:13 +0100

    Board can take a 4x4 board

--- src/test/java/com/pelensky/tictactoe/BoardTest.java
+++ src/test/java/com/pelensky/tictactoe/BoardTest.java
@@ -11,15 +11,18 @@ import static org.junit.Assert.assertEquals;
 public class BoardTest {
 
   private Board board;
+  private Board bigBoard;
 
   @Before
   public void setUp() {
     board = new Board(3);
+    bigBoard = new Board(4);
   }
 
   @Test
   public void boardInitializesWithNineSpaces() {
     assertEquals(9, board.getSpaces().size());
+    assertEquals(16, bigBoard.getSpaces().size());
   }
 
   @Test
